c7c3ee9f211247b9ba80a70e6b090c5604dfb983
Мурзік Васильович <93987936+MurzikVasilyevich@users.noreply.github.com>
2022-04-15 15:24:50 +0300

    Update format

--- words_helper.py
+++ words_helper.py
@@ -39,7 +39,7 @@ def open_ai(query_in):
 
 
 def get_word(pos):
-    words = random.sample(list(wn.all_lemma_names(pos)), 5)
+    words = random.sample(list(wn.all_lemma_names(getattr( wn, pos))), 5)
     return [i.replace("_", " ") for i in words]
 
 
